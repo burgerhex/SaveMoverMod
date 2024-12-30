726d0f7adf9cd2363e126ca6866ce754a31cbcb9
whatsGravity <whatsgravity2@gmail.com>
2024-12-30 10:05:37 -0500

    added button bind for moving saves

--- Source/SaveMoverModuleSettings.cs
+++ Source/SaveMoverModuleSettings.cs
@@ -1,5 +1,10 @@
+using Microsoft.Xna.Framework.Input;
+
 namespace Celeste.Mod.SaveMover;
 
 public class SaveMoverModuleSettings : EverestModuleSettings {
+
+    [DefaultButtonBinding(button: Buttons.LeftTrigger, key: Keys.Tab)]
+    public ButtonBinding SaveMoverBind { get; set; }
 
 }
