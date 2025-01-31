653da90748848394a994932887ae1973a0bd0c7e
burgerhex <40219421+burgerhex@users.noreply.github.com>
2025-01-30 22:48:13 -0800

    Move izumi files, sloppily

--- Source/SaveMoverModule.cs
+++ Source/SaveMoverModule.cs
@@ -86,111 +86,167 @@ public class SaveMoverModule : EverestModule {
         orig(self);
     }
 
-        private static void onFileSelectUpdate(On.Celeste.OuiFileSelect.orig_Update orig, OuiFileSelect self) {
-            // make sure vanilla portraits are loaded (in case the player played a map with a custom Portraits.xml).
-            // GFX.PortraitsSpriteBank = new SpriteBank(GFX.Portraits, Path.Combine("Graphics", "Portraits.xml"));
-            var menuData = self.Get<DataComponent>();
-            if (menuData is null) {
-                self.Add(menuData = new DataComponent());
-            } 
+    private static void onFileSelectUpdate(On.Celeste.OuiFileSelect.orig_Update orig, OuiFileSelect self) {
+        // make sure vanilla portraits are loaded (in case the player played a map with a custom Portraits.xml).
+        // GFX.PortraitsSpriteBank = new SpriteBank(GFX.Portraits, Path.Combine("Graphics", "Portraits.xml"));
+        var menuData = self.Get<DataComponent>();
+        if (menuData is null) {
+            self.Add(menuData = new DataComponent());
+        } 
 
-            if (self.Focused && Settings.SaveMoverBind.Pressed) {
-                if (menuData.IsMoving){
-                    Audio.Play("event:/ui/main/button_toggle_off");
-                    menuData.IsMoving = false;
-                    // Copy to temp
+        if (self.Focused && Settings.SaveMoverBind.Pressed) {
+            if (menuData.IsMoving){
+                Audio.Play("event:/ui/main/button_toggle_off");
+                menuData.IsMoving = false;
+                // Copy to temp
 
-                    // Glob all mod files
-                    string searchDirectory = "./Saves";
-                    DirectoryInfo dir = new DirectoryInfo(searchDirectory);
+                // Glob all mod files
+                string searchDirectory = "./Saves";
+                DirectoryInfo dir = new DirectoryInfo(searchDirectory);
 
-                    List<Tuple<string, string>> movedFiles = new List<Tuple<string, string>>();  
-                    foreach (var file in dir.EnumerateFiles($"{menuData.StartSlot}-*.celeste").Concat(dir.EnumerateFiles($"{menuData.StartSlot}.celeste"))) {
-                        var regex = new Regex(Regex.Escape($"{menuData.StartSlot}"));
-                        string movedPath = regex.Replace(file.Name, $"{self.SlotIndex}", 1);
-                        string tempPath = searchDirectory + $"/temp_{file.Name}";
-                        movedFiles.Add(new Tuple<string, string>(tempPath, searchDirectory + $"/{movedPath}"));
-                        File.Move(searchDirectory + $"/{file.Name}", tempPath);
-                    }
-
-                    if (menuData.StartSlot > self.SlotIndex) {
-                        for (int i = menuData.StartSlot-1; i >= self.SlotIndex; i--) {
-                            foreach(var file in dir.EnumerateFiles($"{i}-*.celeste").Concat(dir.EnumerateFiles($"{i}.celeste"))) {
-                                var regex = new Regex(Regex.Escape($"{i}"));
-                                string movedPath = regex.Replace(file.Name, $"{i+1}", 1);
-                                File.Move(searchDirectory + $"/{file.Name}", searchDirectory + $"/{movedPath}");
-                            }
-                        }
-                    } else if (menuData.StartSlot < self.SlotIndex) {
-                        for (int i = menuData.StartSlot; i < self.SlotIndex; i++) {
-                            foreach(var file in dir.EnumerateFiles($"{i+1}-*.celeste").Concat(dir.EnumerateFiles($"{i+1}.celeste"))) {
-                                var regex = new Regex(Regex.Escape($"{i+1}"));
-                                string movedPath = regex.Replace(file.Name, $"{i}", 1);
-                                File.Move(searchDirectory + $"/{file.Name}", searchDirectory + $"/{movedPath}");
-                            }
-                        }
-                    }
-                    foreach( var filePair in movedFiles) {
-                        File.Move(filePair.Item1, filePair.Item2);
-                    }
-
-                }
-                else if (self.SlotIndex < self.Slots.Length - 1) {
-                    Audio.Play("event:/ui/main/button_toggle_on");
-                    menuData.IsMoving = true;
-                    menuData.StartSlot = self.SlotIndex;
+                // copy the file(s) where we started the move to a temporary file. later we'll copy it to the destination file
+                List<Tuple<string, string>> movedFiles = new List<Tuple<string, string>>();  
+                foreach (var file in dir.EnumerateFiles($"{menuData.StartSlot}-*.celeste").Concat(dir.EnumerateFiles($"{menuData.StartSlot}.celeste"))) {
+                    // move to a temp file
+                    string tempPath = searchDirectory + $"/temp_{file.Name}";
+                    File.Move(searchDirectory + $"/{file.Name}", tempPath);
+                    // store this, and later, move this temp file to the correct end location
+                    var regex = new Regex(Regex.Escape($"{menuData.StartSlot}"));
+                    string movedPath = regex.Replace(file.Name, $"{self.SlotIndex}", 1);
+                    movedFiles.Add(new Tuple<string, string>(tempPath, searchDirectory + $"/{movedPath}"));
                 }
 
+                // if we're moving a file upward (i.e. to a lower index)
+                if (menuData.StartSlot > self.SlotIndex) {
+                    // copy start-1 to start, start-2 to start-1, ..., end to end-1
+                    for (int i = menuData.StartSlot-1; i >= self.SlotIndex; i--) {
+                        foreach(var file in dir.EnumerateFiles($"{i}-*.celeste").Concat(dir.EnumerateFiles($"{i}.celeste"))) {
+                            var regex = new Regex(Regex.Escape($"{i}"));
+                            string movedPath = regex.Replace(file.Name, $"{i+1}", 1);
+                            File.Move(searchDirectory + $"/{file.Name}", searchDirectory + $"/{movedPath}");
+                        }
+                    }
+                } else if (menuData.StartSlot < self.SlotIndex) {
+                    // copy start+1 to start, start+2 to start+1, ..., end to end-1
+                    for (int i = menuData.StartSlot; i < self.SlotIndex; i++) {
+                        foreach(var file in dir.EnumerateFiles($"{i+1}-*.celeste").Concat(dir.EnumerateFiles($"{i+1}.celeste"))) {
+                            var regex = new Regex(Regex.Escape($"{i+1}"));
+                            string movedPath = regex.Replace(file.Name, $"{i}", 1);
+                            File.Move(searchDirectory + $"/{file.Name}", searchDirectory + $"/{movedPath}");
+                        }
+                    }
+                }
+                foreach( var filePair in movedFiles) {
+                    File.Move(filePair.Item1, filePair.Item2);
+                }
+
+
+            // Glob all izumi files
+            string izumiSearchDirectory = "./Saves/izumisQOL/journalStats";
+            DirectoryInfo izumiDir = new DirectoryInfo(izumiSearchDirectory);
+
+            // copy the file(s) where we started the move to a temporary file. later we'll copy it to the destination file
+            List<Tuple<string, string>> movedIzumiFiles = new List<Tuple<string, string>>();
+            foreach (var file in izumiDir.EnumerateFiles($"{menuData.StartSlot}_*.txt"))
+            {
+                // move to a temp file
+                string tempPath = izumiSearchDirectory + $"/temp_{file.Name}";
+                File.Move(izumiSearchDirectory + $"/{file.Name}", tempPath);
+                // store this, and later, move this temp file to the correct end location
+                var regex = new Regex(Regex.Escape($"{menuData.StartSlot}"));
+                string movedPath = regex.Replace(file.Name, $"{self.SlotIndex}", 1);
+                movedIzumiFiles.Add(new Tuple<string, string>(tempPath, izumiSearchDirectory + $"/{movedPath}"));
             }
-            // We're just going to recreate the menu update logic if we're moving
-            if (menuData.IsMoving) {
-                // (self as Oui).Update();
-                if (!self.Focused)  {
-                    menuData.IsMoving = false;
-                    return;
-                }
-                if (!self.SlotSelected) {
-                    if (Input.MenuUp.Pressed && self.SlotIndex > 0) {
-                        Audio.Play("event:/ui/main/savefile_rollover_up");
-                        OuiFileSelectSlot prev = self.Slots[self.SlotIndex - 1];
-                        OuiFileSelectSlot curr = self.Slots[self.SlotIndex];
-                        curr.FileSlot--;
-                        prev.FileSlot++;
-                        self.Slots[self.SlotIndex] = prev;
-                        self.Slots[self.SlotIndex-1] = curr;
-                        self.SlotIndex--;
-                        foreach (OuiFileSelectSlot slot in self.Slots) {
-                            // if (slot == curr) continue;
-                            slot.MoveTo(slot.IdlePosition.X, slot.IdlePosition.Y);
-                        }
-                    }
-                    else if (Input.MenuDown.Pressed && self.SlotIndex < self.Slots.Length - 2)  {
-                        Audio.Play("event:/ui/main/savefile_rollover_down");
-                        OuiFileSelectSlot next = self.Slots[self.SlotIndex + 1];
-                        OuiFileSelectSlot curr = self.Slots[self.SlotIndex];
-                        curr.FileSlot++;
-                        next.FileSlot--;
-                        self.Slots[self.SlotIndex] = next;
-                        self.Slots[self.SlotIndex+1] = curr;
-                        self.SlotIndex++;
-                        foreach (OuiFileSelectSlot slot in self.Slots) {
-                            // if (slot == curr) continue;
-                            slot.MoveTo(slot.IdlePosition.X, slot.IdlePosition.Y);
-                        }
-                    }
-                    else if (Input.MenuCancel.Pressed) {
-                        Audio.Play("event:/ui/main/button_back");
-                         (self as Oui).Overworld.Goto<OuiMainMenu>();
+
+            // if we're moving a file upward (i.e. to a lower index)
+            if (menuData.StartSlot > self.SlotIndex)
+            {
+                // copy start-1 to start, start-2 to start-1, ..., end to end-1
+                for (int i = menuData.StartSlot - 1; i >= self.SlotIndex; i--)
+                {
+                    foreach (var file in izumiDir.EnumerateFiles($"{i}_*.txt"))
+                    {
+                        var regex = new Regex(Regex.Escape($"{i}"));
+                        string movedPath = regex.Replace(file.Name, $"{i + 1}", 1);
+                        File.Move(izumiSearchDirectory + $"/{file.Name}", izumiSearchDirectory + $"/{movedPath}");
                     }
                 }
-                else if (Input.MenuCancel.Pressed && !self.HasSlots && !self.Slots[self.SlotIndex].StartingGame)   {
-                    Audio.Play("event:/ui/main/button_back");
-                     (self as Oui).Overworld.Goto<OuiMainMenu>();
-                }
-            } else {
-                orig(self);
             }
+            else if (menuData.StartSlot < self.SlotIndex)
+            {
+                // copy start+1 to start, start+2 to start+1, ..., end to end-1
+                for (int i = menuData.StartSlot; i < self.SlotIndex; i++)
+                {
+                    foreach (var file in izumiDir.EnumerateFiles($"{i + 1}_*.txt"))
+                    {
+                        var regex = new Regex(Regex.Escape($"{i + 1}"));
+                        string movedPath = regex.Replace(file.Name, $"{i}", 1);
+                        File.Move(izumiSearchDirectory + $"/{file.Name}", izumiSearchDirectory + $"/{movedPath}");
+                    }
+                }
+            }
+            foreach (var filePair in movedIzumiFiles)
+            {
+                File.Move(filePair.Item1, filePair.Item2);
+            }
+
         }
+        else if (self.SlotIndex < self.Slots.Length - 1) {
+                Audio.Play("event:/ui/main/button_toggle_on");
+                menuData.IsMoving = true;
+                menuData.StartSlot = self.SlotIndex;
+            }
+
+        }
+        // We're just going to recreate the menu update logic if we're moving
+        if (menuData.IsMoving) {
+            // (self as Oui).Update();
+            if (!self.Focused)  {
+                menuData.IsMoving = false;
+                return;
+            }
+            if (!self.SlotSelected) {
+                if (Input.MenuUp.Pressed && self.SlotIndex > 0) {
+                    Audio.Play("event:/ui/main/savefile_rollover_up");
+                    OuiFileSelectSlot prev = self.Slots[self.SlotIndex - 1];
+                    OuiFileSelectSlot curr = self.Slots[self.SlotIndex];
+                    curr.FileSlot--;
+                    prev.FileSlot++;
+                    self.Slots[self.SlotIndex] = prev;
+                    self.Slots[self.SlotIndex-1] = curr;
+                    self.SlotIndex--;
+                    foreach (OuiFileSelectSlot slot in self.Slots) {
+                        // if (slot == curr) continue;
+                        slot.MoveTo(slot.IdlePosition.X, slot.IdlePosition.Y);
+                    }
+                }
+                else if (Input.MenuDown.Pressed && self.SlotIndex < self.Slots.Length - 2)  {
+                    Audio.Play("event:/ui/main/savefile_rollover_down");
+                    OuiFileSelectSlot next = self.Slots[self.SlotIndex + 1];
+                    OuiFileSelectSlot curr = self.Slots[self.SlotIndex];
+                    curr.FileSlot++;
+                    next.FileSlot--;
+                    self.Slots[self.SlotIndex] = next;
+                    self.Slots[self.SlotIndex+1] = curr;
+                    self.SlotIndex++;
+                    foreach (OuiFileSelectSlot slot in self.Slots) {
+                        // if (slot == curr) continue;
+                        slot.MoveTo(slot.IdlePosition.X, slot.IdlePosition.Y);
+                    }
+                }
+                else if (Input.MenuCancel.Pressed) {
+                    Audio.Play("event:/ui/main/button_back");
+                        (self as Oui).Overworld.Goto<OuiMainMenu>();
+                }
+            }
+            else if (Input.MenuCancel.Pressed && !self.HasSlots && !self.Slots[self.SlotIndex].StartingGame)   {
+                Audio.Play("event:/ui/main/button_back");
+                    (self as Oui).Overworld.Goto<OuiMainMenu>();
+            }
+        } else {
+            orig(self);
+        }
+    }
 
 
     #endregion
